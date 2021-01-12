09589e911429ce9451dcef1dafa382f54dc1a3ad
benthebear93 <nswve@naver.com>
2021-01-12 23:43:17 +0900

    minor update

--- H_infinity/H_infinity.py
+++ H_infinity/H_infinity.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from numpy.linalg import inv
 import math as m
 import matplotlib as plt
 
@@ -29,25 +30,65 @@ class HInfinityFilter:
         self.xhatarray = []
 
 
-    def predict(self, u=0):
+    def k_predict(self, u):
         self.x = np.dot(self.F, self.x) + np.dot(self.B, u)
         self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
         return self.x
 
-    def update(self, z):
-        nu = y - np.dot(self.H, self.x)  # residual
+    def k_update(self, z):
+        nu = z - np.dot(self.H, self.x)  # residual
         S = self.R + np.dot(self.H, np.dot(self.P, self.H.T))
         K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))  # kalman gain
-        self.x = self.x + np.dot(K, y)  # stat update
+        self.x = self.x + np.dot(K, nu)  # stat update
         I = np.eye(self.n)
         self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), (I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R),
 
-def define_problem(head_ang):
-    dt = 0.03 #need to be changed
-    F = np.array([[1, 0, dt, 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]])
-    H = np.array([0, 0, dt*m.sin(head_ang), dt*m.cos(head_ang)]).reshape(1, 4)
-    Q = np.diag([4, 4, 1, 1])
-    R = np.diag([900, 900])
+    def h_predict(self, u):
+        ##
+
+    def h_update(self, z):
+
+def define_problem():
+    head_ang = m.pi/3
+    tan_theta = m.tan(head_ang)
+    gamma = 40
+    dt = 1 #need to be changed
+    F = np.array([[1, 0, dt, 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]]) # system matrix
+    B = np.array([0, 0, dt*m.sin(head_ang), dt*m.cos(head_ang)]).reshape(1, 4) # Input matrix
+    Q = np.diag([4, 4, 1, 1]) # process noise covariance
+    R = np.diag([900, 900]) # measurement noise covariance
+    R_sqrt = np.sqrt(R)
+    H = np.array([[1,0, 0, 0],[0, 1, 0, 0]]) + inv(R_sqrt)#Normarlized measurement matrix
+    D = 0
+
+    AccelDecelFalg = 1
+
+    P = np.diag([900, 900, 4, 4])
+    Qbar = P # estimation error covariance for H inf
+    Qtilde = P # estimation error covariance for H inf
+
+    x = np.array([0, 0, tan_theta, 1]).reshape(1, 4)
+    h_inf = HInfinityFilter(gamma=gamma, F = F, B = B, H = H, D = D, P = P, Q = Q, R = R)
+    for dt in range(0,120,1):
+        z = H * x # get the noise-corrupted measurement
+        MeasErr = np.randn(z.size)
+        z = z + MeasErr
+        if AccelDecelFalg == 1:
+            if x(3) > 30 or x(4) > 30:
+                AccelDecelFalg = -1
+        else:
+            if x(3) < 5 or x(4) < 5:
+                AccelDecelFalg = -1
+        u = 1 * AccelDecelFalg
+        h_inf.k_predict(u)
+        h_inf.k_update(z)
+
+
+    Pinf = inv(np.eye(4) - Qbar)
+
+
+
+
 
 
 if __name__ == '__main__':
